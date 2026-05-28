489cbb47bd7229c45b521555a5800e3f5b84dfd2
Codewithtechfrenz <techfrenz2025@gmail.com>
2026-05-28 20:13:42 +0530

    Updated base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8881",
+  baseURL: "https://marketingscapers.com/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
